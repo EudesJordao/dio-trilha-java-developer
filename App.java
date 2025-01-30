2af65f3d1ae1a795b700e1f4b6be488338373f3a
Eudes Jordão <82156813+EudesJordao@users.noreply.github.com>
2025-01-30 15:31:12 -0300

    Desafio concluído

---
+++ App.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+     
+        System.out.println("Por favor, digite o número da conta: ");
+        int numeroConta = scanner.nextInt();
+
+        System.out.println("Por favor, digite o número da agência: ");
+        String numeroAgencia = scanner.next();
+        scanner.nextLine();
+
+        System.out.println("Por favor, digite o nome do titular: ");
+        String nomeTitular = scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo da conta: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá, " + nomeTitular + "! Obrigado por abrir sua conta conosco. Sua agência é " + numeroAgencia + ", sua conta é " + numeroConta + " e seu saldo é de R$" + saldo + ".");
+        
+    }
+
+}
