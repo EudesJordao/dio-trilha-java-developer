edd92ec8687b1d90e9263b7194a0928d43bfe73c
Eudes Jordão <82156813+EudesJordao@users.noreply.github.com>
2025-04-06 12:42:34 -0300

    Estudo de Stack

---
+++ Pilhas em Java/pilha_java/src/Main.java
@@ -0,0 +1,22 @@
+import one.digitalInnovation.Pilha;
+import one.digitalInnovation.No;
+
+public class Main {
+    public static void main(String[] args) {
+        Pilha minhaPilha = new Pilha();
+
+        minhaPilha.push(new No(1));
+        minhaPilha.push(new No(2));
+        minhaPilha.push(new No(3));
+        minhaPilha.push(new No(4));
+        minhaPilha.push(new No(5));
+        minhaPilha.push(new No(6));
+
+        minhaPilha.pop();
+        minhaPilha.pop();
+        minhaPilha.pop();
+
+
+        System.out.println(minhaPilha);
+    }
+}
